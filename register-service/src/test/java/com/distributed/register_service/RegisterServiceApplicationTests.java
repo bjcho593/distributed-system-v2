0bb41e6a35cf824f93f24c4f67be943470bb97b3
bjcho593 <155932533+bjcho593@users.noreply.github.com>
2025-06-22 18:26:01 -0500

    Update RegisterServiceApplicationTests.java

--- register-service/src/test/java/com/distributed/register_service/RegisterServiceApplicationTests.java
+++ register-service/src/test/java/com/distributed/register_service/RegisterServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.distributed.register_service;
+package com.distributed.registerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
