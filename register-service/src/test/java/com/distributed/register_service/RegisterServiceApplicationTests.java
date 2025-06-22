3bae8abee33544d43e1be9a6284e312d40fada80
bjcho593 <155932533+bjcho593@users.noreply.github.com>
2025-06-22 18:49:54 -0500

    Update RegisterServiceApplicationTests.java

--- register-service/src/test/java/com/distributed/register_service/RegisterServiceApplicationTests.java
+++ register-service/src/test/java/com/distributed/register_service/RegisterServiceApplicationTests.java
@@ -2,6 +2,9 @@ package com.distributed.registerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest
 class RegisterServiceApplicationTests {
