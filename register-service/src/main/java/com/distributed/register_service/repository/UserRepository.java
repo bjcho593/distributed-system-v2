526bb4767855ef4b29a4b3dff97b460ba3c34567
bjcho593 <155932533+bjcho593@users.noreply.github.com>
2025-06-22 16:53:49 -0500

    Create UserRepository.java

---
+++ register-service/src/main/java/com/distributed/register_service/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.distributed.registerservice.repository;
+
+import com.distributed.registerservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+}
