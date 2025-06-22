57d562ef3849632ed104ca7767d9ecb456f83073
bjcho593 <155932533+bjcho593@users.noreply.github.com>
2025-06-22 18:03:57 -0500

    Update RegisterServiceApplication.java

--- register-service/src/main/java/com/distributed/register_service/RegisterServiceApplication.java
+++ register-service/src/main/java/com/distributed/register_service/RegisterServiceApplication.java
@@ -2,6 +2,7 @@ package com.distributed.registerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.distributed.registerservice")
