6a69c5c12a0d82fd9c15b14b09ba868fc024bf9a
bjcho593 <155932533+bjcho593@users.noreply.github.com>
2025-06-22 20:28:56 -0500

    Delete password-recovery-service/main.py

--- password-recovery-service/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.routes import recovery
-from dotenv import load_dotenv
-
-load_dotenv()
-
-app = FastAPI()
-app.include_router(recovery.router, prefix="/recovery")
